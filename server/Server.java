413b8541b3c3b188fa2a42ded1cb55af5c07c692
bourgondaries <kevinrs@stud.ntnu.no>
2015-02-20 16:34:29 +0100

    Add some security routines, they are not active yet

--- server/Server.java
+++ server/Server.java
@@ -10,7 +10,27 @@ public class Server
 			while (true)
 			{
 				// init
-
+				javax.net.ServerSocketFactory socket_factory = javax.net.ssl.SSLServerSocketFactory.getDefault();
+				java.net.ServerSocket ssl_socket = socket_factory.createServerSocket(7777);
+				try
+				{
+					java.security.KeyPairGenerator keygen = java.security.KeyPairGenerator.getInstance("DSA", "SUN");
+					java.security.SecureRandom random = java.security.SecureRandom.getInstance("SHA1PRNG", "SUN");
+					keygen.initialize (2048, random);
+					java.security.KeyPair pair = keygen.generateKeyPair();
+					java.security.PrivateKey private_key = pair.getPrivate();
+					java.security.PublicKey public_key = pair.getPublic();
+					
+					System.out.println(public_key.getEncoded());
+				}
+				catch (java.security.NoSuchAlgorithmException exc_obj)
+				{
+					System.out.println(exc_obj);
+				}
+				catch (java.security.NoSuchProviderException exc_obj)
+				{
+					System.out.println(exc_obj);
+				}
 				java.net.ServerSocket server_socket = new java.net.ServerSocket(7777);
 				System.out.println("Waiting for acceptance");
 				java.net.Socket client_socket = server_socket.accept();
