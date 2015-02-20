63898031807e07ace9f2d954269d0d86e0a24209
bourgondaries <kevinrs@stud.ntnu.no>
2015-02-20 14:50:54 +0100

    Derp

--- main/main.java
+++ main/main.java
@@ -8,16 +8,12 @@ public class Main
 		System.out.println("herp");
 		System.out.println("Hello world!");
 		what();
-		System.out.println("gfhghdfjgh")
-		System.out.println("halla");
-		System.out.println("hey hey hey!");
-		System.out.println("MAULI ER BEST!");
 	}
 
 	public static void what()
 	{
 		System.out.println("Let's try it!");
-		// Rolig nå
 		System.out.println("HEY HEY HEY");
 	}
-}	// Comment
+}
+
