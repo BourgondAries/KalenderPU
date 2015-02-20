c0f6c676edd166c950f342f0223ee9d505126d48
bourgondaries <kevinrs@stud.ntnu.no>
2015-02-20 14:56:43 +0100

    Fix syntax error

---
+++ main/Main.java
@@ -0,0 +1,28 @@
+package main;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		System.out.println("derp!");
+		System.out.println("herp");
+		System.out.println("Hello world!");
+		System.out.println("gfhghdfjgh");
+		System.out.println("halla");
+		System.out.println("hey hey hey!");
+		System.out.println("MAULI ER BEST!");
+		System.out.println("Hans er retard i dag");
+		what();
+	}
+
+	public static void what()
+	{
+		System.out.println("Let's try it!");
+		System.out.println("HEY HEY HEY");
+		for (int i = 0; i < 1000; ++i)
+		{
+			System.out.println("Hans er retard for " + String.valueOf(i) + ". gang i dag.");
+		}	
+	}
+}
+
