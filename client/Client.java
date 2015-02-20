c5e72a4af6bf21184567ef62da168c5c2d8b7607
bourgondaries <kevinrs@stud.ntnu.no>
2015-02-20 18:32:57 +0100

    Add succesful encryption

--- client/Client.java
+++ client/Client.java
@@ -8,14 +8,64 @@ public class Client
 		{
 			java.net.Socket client_socket = new java.net.Socket("localhost", 7777);
 			java.io.OutputStream out_stream = client_socket.getOutputStream();
+			java.io.InputStream in_stream = client_socket.getInputStream();
+
+			java.security.PublicKey public_key = getPublicKeyFromServer(in_stream);
+
 			java.util.Scanner sc = new java.util.Scanner(System.in);
 			String write = sc.nextLine();
-			out_stream.write(write.getBytes());
+			try
+			{
+				out_stream.write(encrypt(write.getBytes(), public_key, "RSA/ECB/PKCS1PADDING"));
+			}
+			catch (Exception exc_obj)
+			{
+				exc_obj.printStackTrace();
+			}
 			out_stream.flush();
 		}
 		catch (java.io.IOException exc_obj)
 		{
 			System.out.println(exc_obj);
 		}
+	}
+
+	public static java.security.PublicKey getPublicKeyFromServer(java.io.InputStream in_stream)
+	{
+		byte[] bytes = new byte[2048];
+		try
+		{
+			int number = in_stream.read(bytes);
+			if (number != 2048)
+				System.out.println("Wrong number of bytes!" + number);
+			bytes = java.util.Arrays.copyOf(bytes, number);
+			System.out.println(new String(bytes));
+			try
+			{
+				java.security.spec.X509EncodedKeySpec pubkey_spec = new java.security.spec.X509EncodedKeySpec(bytes);
+				java.security.KeyFactory key_factory = java.security.KeyFactory.getInstance("RSA");
+				return key_factory.generatePublic(pubkey_spec);
+			}
+			catch (java.security.NoSuchAlgorithmException exc_obj)
+			{
+				System.out.println(exc_obj);
+			}
+			catch (java.security.spec.InvalidKeySpecException exc_obj)
+			{
+				System.out.println(exc_obj);
+			}
+		}
+		catch (java.io.IOException exc_obj)
+		{
+			System.out.println(exc_obj);
+		}
+		return null;
+	}
+
+	private static byte[] encrypt(byte[] inpBytes, java.security.PublicKey key, String xform) throws Exception
+	{
+	    javax.crypto.Cipher cipher = javax.crypto.Cipher.getInstance(xform);
+	    cipher.init(javax.crypto.Cipher.ENCRYPT_MODE, key);
+	    return cipher.doFinal(inpBytes);
 	}
 }
--- server/Server.java
+++ server/Server.java
@@ -10,37 +10,28 @@ public class Server
 			while (true)
 			{
 				// init
-				javax.net.ServerSocketFactory socket_factory = javax.net.ssl.SSLServerSocketFactory.getDefault();
-				java.net.ServerSocket ssl_socket = socket_factory.createServerSocket(7777);
-				try
-				{
-					java.security.KeyPairGenerator keygen = java.security.KeyPairGenerator.getInstance("DSA", "SUN");
-					java.security.SecureRandom random = java.security.SecureRandom.getInstance("SHA1PRNG", "SUN");
-					keygen.initialize (2048, random);
-					java.security.KeyPair pair = keygen.generateKeyPair();
-					java.security.PrivateKey private_key = pair.getPrivate();
-					java.security.PublicKey public_key = pair.getPublic();
-					
-					System.out.println(public_key.getEncoded());
-				}
-				catch (java.security.NoSuchAlgorithmException exc_obj)
-				{
-					System.out.println(exc_obj);
-				}
-				catch (java.security.NoSuchProviderException exc_obj)
-				{
-					System.out.println(exc_obj);
-				}
 				java.net.ServerSocket server_socket = new java.net.ServerSocket(7777);
 				System.out.println("Waiting for acceptance");
+
 				java.net.Socket client_socket = server_socket.accept();
 				System.out.println("Got acceptance");
+
 				java.io.InputStream input_from_client = client_socket.getInputStream();
 				java.io.OutputStream output_to_client = client_socket.getOutputStream();
+
+				java.security.PrivateKey private_key = sendPublicKeyToClient(output_to_client);
+
 				byte[] bytes = new byte[1000];
 				int code = input_from_client.read(bytes);
 				bytes = java.util.Arrays.copyOf(bytes, code);
-				System.out.println(new String(bytes));
+				try
+				{
+					System.out.println(new String(decrypt(bytes, private_key, "RSA/ECB/PKCS1PADDING")));
+				}
+				catch (Exception exc_obj)
+				{
+					exc_obj.printStackTrace();
+				}
 				System.out.println("read: " + code + " bytes");
 				server_socket.close();
 			}		
@@ -49,5 +40,47 @@ public class Server
 		{
 			System.out.println(exc_obj);
 		}
+		
+	}
+
+	public static java.security.KeyPair getNewKeyPair()
+	{
+		try
+		{
+			java.security.KeyPairGenerator keygen = java.security.KeyPairGenerator.getInstance("RSA");
+			//java.security.SecureRandom random = java.security.SecureRandom.getInstance("SHA1PRNG", "SUN");
+			keygen.initialize(2048/*, random*/);
+			return keygen.generateKeyPair();
+		}
+		catch (java.security.NoSuchAlgorithmException exc_obj)
+		{
+			System.out.println(exc_obj);
+		}
+		return null;
+	}
+
+	public static java.security.PrivateKey sendPublicKeyToClient(java.io.OutputStream output_to_client)
+	{
+		try
+		{
+			java.security.KeyPair pair = getNewKeyPair();
+			java.security.PrivateKey private_key = pair.getPrivate();
+			java.security.PublicKey public_key = pair.getPublic();
+			output_to_client.write(public_key.getEncoded());
+			System.out.println(new String(public_key.getEncoded()));
+			return private_key;
+		}
+		catch (java.io.IOException exc_obj)
+		{
+			System.out.println(exc_obj);
+		}
+		return null;
+	}
+
+	private static byte[] decrypt(byte[] inpBytes, java.security.PrivateKey key, String xform) throws Exception
+	{
+	    javax.crypto.Cipher cipher = javax.crypto.Cipher.getInstance(xform);
+	    cipher.init(javax.crypto.Cipher.DECRYPT_MODE, key);
+	    return cipher.doFinal(inpBytes);
 	}
 }
