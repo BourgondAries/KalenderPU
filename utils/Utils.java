a6c4814f07725821055c9525bd12124a76a1a256
bourgondaries <kevinrs@stud.ntnu.no>
2015-02-20 23:16:33 +0100

    Add a lot of handling code for certificates

--- utils/Utils.java
+++ utils/Utils.java
@@ -7,6 +7,12 @@ public class Utils
 		return new java.util.Scanner(new java.io.File(filename)).useDelimiter("\\Z").next();
 	}
 
+	public static byte[] fileToBytes(String filename) throws java.io.IOException
+	{
+		java.nio.file.Path path = java.nio.file.Paths.get(filename);
+      	return java.nio.file.Files.readAllBytes(path);
+	}
+
 	public static byte[] encrypt(byte[] inp_bytes, java.security.PublicKey key) throws Exception
 	{
 		return encrypt(inp_bytes, key, Configuration.settings.get("xform"));
@@ -49,5 +55,55 @@ public class Utils
 			System.out.println(exc_obj);
 		}
 		return null;
+	}
+
+	public static java.security.PublicKey bytesToPublicKey(byte[] bytes)
+	{
+		try
+		{
+			java.security.spec.X509EncodedKeySpec pubkey_spec = new java.security.spec.X509EncodedKeySpec(bytes);
+			java.security.KeyFactory key_factory = java.security.KeyFactory.getInstance(Configuration.settings.get("keypairgen"));
+			return key_factory.generatePublic(pubkey_spec);
+		}
+		catch (java.security.NoSuchAlgorithmException exc_obj)
+		{
+			System.out.println(exc_obj);
+		}
+		catch (java.security.spec.InvalidKeySpecException exc_obj)
+		{
+			System.out.println(exc_obj);
+		}
+		return null;
+	}
+
+	public static java.security.PrivateKey bytesToPrivateKey(byte[] bytes)
+	{
+		try
+		{
+			java.security.spec.PKCS8EncodedKeySpec pubkey_spec = new java.security.spec.PKCS8EncodedKeySpec(bytes);
+			java.security.KeyFactory key_factory = java.security.KeyFactory.getInstance(Configuration.settings.get("keypairgen"));
+			return key_factory.generatePrivate(pubkey_spec);
+		}
+		catch (java.security.NoSuchAlgorithmException exc_obj)
+		{
+			System.out.println(exc_obj);
+		}
+		catch (java.security.spec.InvalidKeySpecException exc_obj)
+		{
+			System.out.println(exc_obj);
+		}
+		return null;
+	}
+
+	public static java.security.PublicKey getServerPublicKey() throws java.io.IOException
+	{
+		byte[] pubkey_raw = fileToBytes(Configuration.settings.get("ServerPublicKeyFile"));
+		return bytesToPublicKey(pubkey_raw);
+	}
+
+	public static java.security.PrivateKey getServerPrivateKey() throws java.io.IOException
+	{
+		byte[] pubkey_raw = fileToBytes(Configuration.settings.get("ServerPrivateKeyFile"));
+		return bytesToPrivateKey(pubkey_raw);
 	}
 }
