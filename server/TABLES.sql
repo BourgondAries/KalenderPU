dfe4171f84621c0a53427c3b1ac852b0977562dc
Edvard <hafskolt@gmail.com>
2015-02-21 12:58:28 +0100

    TABLES opptdatert.

--- server/TABLES.sql
+++ server/TABLES.sql
@@ -1,50 +1,60 @@
 
-CREATE TABLE USER(
-userid  	int 			NOT NULL AUTO_INCREMENT,
-rank 		int 			NOT NULL,
-fname 		varchar(255) 	NOT NULL,
-lname 		varchar(255) 	NOT NULL,
-hashedPW 	varchar()		NOT NULL,
-PRIMARY KEY userid 
+CREATE TABLE User(
+userId  	int 			NOT NULL AUTO_INCREMENT,
+rank 		int,
+fname 		varchar(255),
+lname 		varchar(255),
+hashedPW 	varchar(255),
+PRIMARY KEY (userid) 
 );
 
-CREATE TABLE EVENT()
+CREATE TABLE Event()
 eventId 	int NOT NULL AUTO_INCREMENT,
 message 	varchar(255),
 time 		timestamp,
-);
-
-CREATE TABLE BOOKING( 
-bookingId		int NOT NULL AUTO_INCREMENT)
-
-CREATE TABLE GROUP(
-groupid 	int 		NOT NULL,
-rank		int			NOT NULL,
-groupName	varchar(255),
-PRIMARY KEY (groupid),
+PRIMARY KEY (eventId)
+FOREIGN KEY (userid) REFERENCES USER(userid)
 FOREIGN KEY (groupid) REFERENCES GROUP(groupid)
 );
 
-CREATE TABLE GROUPMENBER(
+CREATE TABLE Booking( 
+bookingId		int NOT NULL AUTO_INCREMENT,
+adminId	int NOT NULL,
+description		varchar(255),
+bookingName		varchar(255),
+PRIMARY KEY (bookingId)
+FOREIGN KEY (roomId) REFERENCES ROOM(roomId)
+FOREIGN KEY (adminId) REFERENCES USER(userid)
+)
+
+CREATE TABLE Group(
+groupId 	int 		NOT NULL,
+rank		int			NOT NULL,
+groupName	varchar(255), 
+parrentGroupId int CHECK (groupId != parrentGroupId)
+PRIMARY KEY (groupid),
+FOREIGN KEY (parrentGroupId) REFERENCES GROUP(groupId)
+);
+
+CREATE TABLE Groupmenber(
 userid		int		NOT NULL,
 groupid		int		NOT NULL,
 PRIMARY KEY (userid, groupid),
 FOREIGN KEY (userid) REFERENCES USER (userid),
 FOREIGN KEY (groupid) REFERENCES GROUP (groupid)
 );
+CREATE TABLE Invitiation(
+)
 
-CREATE TABLE NOTIFICATION(
+CREATE TABLE Notification(
 notificationId 		int NOTNULL AUTO_INCREMENT,
 message 			varchar(255),
 duration			varchar(255),
-seen 				boolean,
+seen 				bit,
 );
-CREATE TABLE ROOM(
+
+CREATE TABLE Room(
 roomId		int NOT NULL AUTO_INCREMENT,
 size 		int
 location	varchar(255),
 );
-CREATE TABLE BOOKING(
-bookingId 	int 		NOT NULL,
-bookingName	varchar(255),
-)
