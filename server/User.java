8dba9c04344f465bc69ec2052a3306cbcf484431
oaramsda <olea.ramsdal@gmail.com>
2015-02-27 15:56:45 +0100

    Changes in user.java

--- server/User.java
+++ server/User.java
@@ -20,17 +20,18 @@ public class User
 		return this;
 	}
 
-<<<<<<< HEAD
+
 	void saveUser(){
+		return;
+	}
 
 		
-=======
 	public void getUserFromDatabase(int user_id)
 	{
 
 		Database db = new Database(utils.Configuration.settings.get("DBConnection"));
 
 		return ;
->>>>>>> 2e9392cba030e1fd5e74ab2505d6f001b17264ed
+
 	}
 }
