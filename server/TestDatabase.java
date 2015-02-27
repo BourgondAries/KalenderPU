d4617a5b7daa3fbed5bb1e2f8933eed6120149d5
oaramsda <olea.ramsdal@gmail.com>
2015-02-27 15:46:45 +0100

    add stuff to testDatabase.java

--- server/TestDatabase.java
+++ server/TestDatabase.java
@@ -15,8 +15,9 @@ public class TestDatabase
 		assertEquals(db.execute(userR.username,"gerp", "query"),"Invalid password for user '" + userR.username + "'.");
 
 	}
+
 	@org.junit.Test
-	public static void testUsernameNotExcisting() throws java.io.IOException
+	public static void testUsernameNotExisting() throws java.io.IOException
 	{	
 		utils.Configuration.loadDefaultConfiguration();
 		//User userR = new User(0,1,"root","","",PasswordHash.createHash("root"));
@@ -25,20 +26,37 @@ public class TestDatabase
 	}
 
 	@org.junit.Test
-	public static void testRegisterCommand()  throws java.security.NoSuchAlgorithmException, java.security.spec.InvalidKeySpecException,java.io.IOException
+	public static void testRegisterCommand()  throws java.security.NoSuchAlgorithmException, java.security.spec.InvalidKeySpecException,java.io.IOException, java.sql.SQLException
 	{
 		utils.Configuration.loadDefaultConfiguration();
-		User userR = new User(0,1,"root","","",PasswordHash.createHash("root"));
+		User userR = new User(0,1,"root", "" ,"",PasswordHash.createHash("root"));
 		Database db = new Database(utils.Configuration.settings.get("DBConnection"));
 		String rndStr = utils.Utils.makeRandomString(8);
-		db.executeWithValidUser(userR, "register\\ "+ rndStr +"\\ 1\\ TestFirst\\ TestLast\\ 12345");
+		System.out.println(rndStr);
+		db.executeWithValidUser(userR, 
+							utils.Configuration.settings.getAndEscape("RegisterCommand")
+							+ " "
+							+ utils.Utils.escapeSpaces(rndStr)
+							+ " "
+							+ utils.Utils.escapeSpaces("1")
+							+ " "
+							+ utils.Utils.escapeSpaces("TestFname")
+							+ " "
+							+ utils.Utils.escapeSpaces("TestLname")
+							+ " "
+							+ utils.Utils.escapeSpaces("12345"));
+
+		String result = db.runQuery("SELECT COUNT(*) FROM systemUser WHERE username = '" + rndStr + "'");
+		System.out.println(result);
 	}
 
 	public static void main(String[] args)
-	{	try
+	{
+		utils.Configuration.verbose_mode = true;
+		try
 		{
 			testWrongPasswordWhenExecute();
-			testUsernameNotExcisting();
+			testUsernameNotExisting();
 			testRegisterCommand();
 		}
 		catch(java.security.NoSuchAlgorithmException nsae)
@@ -50,6 +68,10 @@ public class TestDatabase
 			//TODOjava.io.IOException e
 		}
 		catch(java.io.IOException e)
+		{
+
+		}
+		catch(java.lang.Exception e)
 		{
 
 		}	
