3f9a849056e14a2bcadae809853fb67774fc0a41
BourgondAries <macocio@gmail.com>
2015-02-28 15:11:27 +0100

    Normalize TestDatabase

--- server/TestDatabase.java
+++ server/TestDatabase.java
@@ -9,10 +9,10 @@ public class TestDatabase
 	public static void testWrongPasswordWhenExecute() throws java.security.NoSuchAlgorithmException, java.security.spec.InvalidKeySpecException,java.io.IOException
 	{	
 		utils.Configuration.loadDefaultConfiguration();
-		User userR = new User(0,1,"root","","",PasswordHash.createHash("root"));
+		User test_user = new User(0, 1, "root", "", "", PasswordHash.createHash("root"));
 		Database db = new Database(utils.Configuration.settings.get("DBConnection"));
 		//assertEquals(0,1);
-		assertEquals(db.execute(userR.username,"gerp", "query"),"Invalid password for user '" + userR.username + "'.");
+		assertEquals(db.execute(test_user.username, "gerp", "query"), "Invalid password for user '" + test_user.username + "'.");
 
 	}
 
@@ -20,34 +20,37 @@ public class TestDatabase
 	public static void testUsernameNotExisting() throws java.io.IOException
 	{	
 		utils.Configuration.loadDefaultConfiguration();
-		//User userR = new User(0,1,"root","","",PasswordHash.createHash("root"));
+		//User test_user = new User(0,1,"root","","",PasswordHash.createHash("root"));
 		Database db = new Database(utils.Configuration.settings.get("DBConnection"));
-		assertEquals(db.execute("nonexistingname", "pass", "query"),"Login username 'nonexistingname' does not exist.");
+		assertEquals(db.execute("nonexistingname", "pass", "query"), "Login username 'nonexistingname' does not exist.");
 	}
 
 	@org.junit.Test
 	public static void testRegisterCommand()  throws java.security.NoSuchAlgorithmException, java.security.spec.InvalidKeySpecException,java.io.IOException, java.sql.SQLException
 	{
 		utils.Configuration.loadDefaultConfiguration();
-		User userR = new User(0,1,"root", "" ,"",PasswordHash.createHash("root"));
+		User test_user = new User(0,1,"root", "", "", PasswordHash.createHash("root"));
 		Database db = new Database(utils.Configuration.settings.get("DBConnection"));
-		String rndStr = utils.Utils.makeRandomString(8);
-		//System.out.println(rndStr);
-		db.executeWithValidUser(userR, 
-							utils.Configuration.settings.getAndEscape("RegisterCommand")
-							+ " "
-							+ utils.Utils.escapeSpaces(rndStr)
-							+ " "
-							+ utils.Utils.escapeSpaces("1")
-							+ " "
-							+ utils.Utils.escapeSpaces("TestFname")
-							+ " "
-							+ utils.Utils.escapeSpaces("TestLname")
-							+ " "
-							+ utils.Utils.escapeSpaces("12345"));
+		String random_string = utils.Utils.makeRandomString(8);
+		//System.out.println(random_string);
+		db.executeWithValidUser
+		(
+			test_user, 
+			utils.Configuration.settings.getAndEscape("RegisterCommand")
+			+ " "
+			+ utils.Utils.escapeSpaces(random_string)
+			+ " "
+			+ utils.Utils.escapeSpaces("1")
+			+ " "
+			+ utils.Utils.escapeSpaces("TestFname")
+			+ " "
+			+ utils.Utils.escapeSpaces("TestLname")
+			+ " "
+			+ utils.Utils.escapeSpaces("12345")
+		);
 
-		String result = db.runQuery("SELECT COUNT(*) FROM systemUser WHERE username = '" + rndStr + "'");
-		assertEquals(Integer.parseInt("" + result.charAt(0)),1);
+		String result = db.runQuery("SELECT COUNT(*) FROM systemUser WHERE username = '" + random_string + "'");
+		assertEquals(Integer.parseInt("" + result.charAt(0)), 1);
 	}
 
 	public static void main(String[] args)
@@ -71,7 +74,7 @@ public class TestDatabase
 		{
 
 		}
-		catch(java.lang.Exception e)
+		catch(Exception e)
 		{
 
 		}	
