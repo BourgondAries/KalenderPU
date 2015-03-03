880b8f5e0677c24882cb2d95885d7588b98cbf24
maulith <mauli.thavarajan@gmail.com>
2015-03-03 13:24:05 +0100

    Lager en ny klasse GroupSettings

--- server/Group.java
+++ server/Group.java
@@ -8,7 +8,7 @@ public class Group{
 
 	public int groupID, groupRank;
 	public String groupName;
-	public User groupAdmin; //hvor staar dette om i SQL?
+	public User groupAdmin, groupMember; //hvor staar dette om i SQL?
 	public ArrayList<User> groupMembers;
 	public ArrayList<Group> subGroups;
 	private Database database;	
@@ -20,15 +20,15 @@ public class Group{
 	this.groupRank = groupRank;
 	this.groupName = groupName;
 	this.groupAdmin = groupAdmin;
+	this.groupMember = groupMember;
 	Database db = new Database(utils.Configuration.settings.get("DBConnection"));
-	//Statement stmt = new con.createStatement();
 	ResultSet rs = new ResultSet();
 	PreparedStatement prepStatement = new PreparedStatement();
 	}
 
 
 	//save- og get metoder
-	public Group getGroup(int groupID){
+	public static Group getGroup(int groupID){
 		prepStatement = db.prepareStatement("SELECT * FROM SystemGroup WHERE groupID =?");
 		prepStatement.setString(1, groupID);
 		rs = prepStatement.executeQuery(prepStatement);
@@ -44,14 +44,21 @@ public class Group{
 		prepStatement.setString(3, parts.get(2));
 		prepStatement.setString(4, parts.get(3));
 		rs = prepStatement.executeQuery(); //hvordan execute'er man disse?
-		return rs;
 	}
 
-	public void removeGroup(int groupID, String groupName, User groupAdmin){
+	public static void removeGroup(int groupID, String groupName, User groupMember){
 		query = query.substring(query.indexOf(" ") + 1);
 		java.util.ArrayList<String> parts = utils.Utils.splitAndUnescapeString(query);
-		prepStatement = db.prepareStatement("INSERT INTO SystemGroup (groupID, groupRank, groupName, groupAdmin) VALUES (?, ?, ?, ?)", java.sql.ResultSet.TYPE_SCROLL_INSENSITIVE, java.sql.ResultSet.CONCUR_READ_ONLY);
+		if (user == Group.groupAdmin) {
+			prepStatement = db.prepareStatement("DELETE * FROM SystemGroup (groupID, groupName, groupAdmin) WHERE VALUES (?, ?, ?, ?)", java.sql.ResultSet.TYPE_SCROLL_INSENSITIVE, java.sql.ResultSet.CONCUR_READ_ONLY);
+
+		}
+		else{
+			String errMessage = "You are not permitted to delete this group"
+		}
 		
+
+
 	}
 
 
