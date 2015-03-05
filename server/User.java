7a660627c8fd4af3f99a6c3fc864e0016d76f2f8
Edvard <hafskolt@gmail.com>
2015-03-05 10:56:18 +0100

    God morgen

--- server/User.java
+++ server/User.java
@@ -6,71 +6,78 @@ import java.sql.PreparedStatement;
 
 public class User
 {
-	public int systemUserI, rank;
+	public int systemUserId, rank;
 	public String username, fname, lname, hashedPW;
+	Database db;
+	ResultSet rs;
+	PreparedStatement prepStatement;
 
 	public User(int systemUserId, int rank, String username, String fname, String lname, String hashedPW)
 	{
-		this.systemUserId = systemUserId
-;
+		this.systemUserId = systemUserId;
 		this.rank = rank;
 		this.username = username;
 		this.fname = fname;
 		this.lname = lname;
 		this.hashedPW = hashedPW;
 
-		Database db = new Database(utils.Configuration.settings.get("DBConnection"));
-		ResultSet rs = new ResultSet();
-		PreparedStatement prepStatement = new PreparedStatement();
+		db = new Database(utils.Configuration.settings.get("DBConnection"));
+		rs = new ResultSet();
+		prepStatement = new PreparedStatement();
+	
 	}
 
 	public static User getUser(int systemUserId)
 	{
 		prepStatement = db.prepareStatement("SELECT * FROM SystemUser WHERE systemUserId =?");
 		prepStatement.setString(1, groupID);
+		rs = prepStatement.executeQuery(prepStatement);
 	}
-	public static Group getGroup(int groupID){
+	
+	public static Group getGroup(int groupID)
+	{
 		prepStatement = db.prepareStatement("SELECT * FROM SystemGroup WHERE groupID =?");
 		prepStatement.setString(1, groupID);
 		rs = prepStatement.executeQuery(prepStatement);
 		return rs;
 	}
 
-	void saveUser(){
+	void saveUser()
+	{
 		assertEquals(db.execute("nonexistingname", "pass", "query"), "Login username 'nonexistingname' does not exist.");
 		
 		ArrayList<String> parts = utils.Utils.splitAndUnescapeString(query);
 		java.sql.PreparedStatement statement = connection.prepareStatement
-					(
-						"INSERT INTO SystemUser (username, rank, fname, lname, hashedPW) VALUES (?, ?, ?, ?, ?)", 
-						java.sql.ResultSet.TYPE_SCROLL_INSENSITIVE, java.sql.ResultSet.CONCUR_READ_ONLY
-					);
-					statement.execute();
+		(
+			"INSERT INTO SystemUser (username, rank, fname, lname, hashedPW) VALUES (?, ?, ?, ?, ?)", 
+			java.sql.ResultSet.TYPE_SCROLL_INSENSITIVE, java.sql.ResultSet.CONCUR_READ_ONLY
+		);
 
+		statement.execute();
+
+
+	public static void removeSystemUser(SystemUser systemUser, int systemUserId, String username)
+	{
+			String query = query.substring(query.indexOf(" ") + 1);
+			ArrayList<String> parts = utils.Utils.splitAndUnescapeString(query);
+			
+				prepStatement = db.prepareStatement
+				(
+					"DELETE * FROM SystemUser (systemUserId, rank, username, lname, fname, hashedPW) WHERE VALUES (?, ?, ?)", 
+					java.sql.ResultSet.TYPE_SCROLL_INSENSITIVE, java.sql.ResultSet.CONCUR_READ_ONLY
+				);
+	
 	}
 
 
-	public static void removeGroup(Group gruppe, int groupID, String groupName, User groupMember){
-			query = query.substring(query.indexOf(" ") + 1);
-			java.util.ArrayList<String> parts = utils.Utils.splitAndUnescapeString(query);
-			if (user == Group.groupAdmin) {
-				prepStatement = db.prepareStatement("DELETE * FROM SystemGroup (groupID, groupName, groupAdmin) WHERE VALUES (?, ?, ?)", java.sql.ResultSet.TYPE_SCROLL_INSENSITIVE, java.sql.ResultSet.CONCUR_READ_ONLY);
-				gruppe.subGroups.re
-			}
-			else {
-				String errMessage = "You are not permitted to delete this group";
-
-			}
-			
-
-
 		
-	public void getUserFromDatabase(int systemUserId)
+	public void loadSystemUser(int systemUserId)
 	{
 
-		Database db = new Database(utils.Configuration.settings.get("DBConnection"));
+		db = new Database(utils.Configuration.settings.get("DBConnection"));
 
 		return ;
 
 	}
+
 }
