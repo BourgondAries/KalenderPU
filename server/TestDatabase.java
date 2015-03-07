743586ee39ff8a8abc3070a35f3106d3df7eef22
BourgondAries <macocio@gmail.com>
2015-03-07 13:32:57 +0100

    Remove merge conflicts from test db and remove wrong usage of database resultToString

--- server/TestDatabase.java
+++ server/TestDatabase.java
@@ -5,20 +5,11 @@ import static org.junit.Assert.*;
 public class TestDatabase
 {
 	Database db = null;
-	User rand_user = null;
+	User random_user = null;
 	User user_root = null;
 
 
 	@org.junit.Test
-<<<<<<< HEAD
-	public void testWrongPasswordWhenExecute() throws java.security.NoSuchAlgorithmException, java.security.spec.InvalidKeySpecException,java.io.IOException
-	{	
-		utils.Configuration.loadDefaultConfiguration();
-		User userR = new User(0,1,"root","","",PasswordHash.createHash("root"));
-		Database db = new Database(utils.Configuration.settings.get("DBConnection"));
-		//assertEquals(0,1);
-		assertEquals(db.execute(userR.username,"gerp", "query"),"Invalid password for user '" + userR.username + "'.");
-=======
 	public void testWrongPasswordWhenExecute() throws java.security.NoSuchAlgorithmException, java.security.spec.InvalidKeySpecException, java.io.IOException
 	{	
 		utils.Configuration.loadDefaultConfiguration();
@@ -26,44 +17,13 @@ public class TestDatabase
 		Database db = new Database(utils.Configuration.settings.get("DBConnection"));
 		db.execute(user_root.username, "password_gerp", utils.Configuration.settings.get("PassCheck"));
 		assertTrue(db.getStatus(Database.Status.INCORRECT_PASSWORD));
->>>>>>> 81b82de93d1347b2c2ee06c6994984d352d27618
-
 	}
 
 	@org.junit.Test
 	public void testUsernameNotExisting() throws java.io.IOException
 	{	
 		utils.Configuration.loadDefaultConfiguration();
-<<<<<<< HEAD
 		//User userR = new User(0,1,"root","","",PasswordHash.createHash("root"));
-		Database db = new Database(utils.Configuration.settings.get("DBConnection"));
-		assertEquals(db.execute("nonexistingname", "password", "query"),"Login username 'nonexistingname' does not exist.");
-	}
-
-	@org.junit.Test
-	public void testRegisterCommand()  throws java.security.NoSuchAlgorithmException, java.security.spec.InvalidKeySpecException,java.io.IOException, java.sql.SQLException
-	{
-		utils.Configuration.loadDefaultConfiguration();
-		User userR = new User(0,1,"root", "" ,"",PasswordHash.createHash("root"));
-		Database db = new Database(utils.Configuration.settings.get("DBConnection"));
-		String rndStr = utils.Utils.makeRandomString(8);
-		//System.out.println(rndStr);
-		db.executeWithValidUser(userR, 
-							utils.Configuration.settings.getAndEscape("RegisterCommand")
-							+ " "
-							+ utils.Utils.escapeSpaces(rndStr)
-							+ " "
-							+ utils.Utils.escapeSpaces("1")
-							+ " "
-							+ utils.Utils.escapeSpaces("TestFname")
-							+ " "
-							+ utils.Utils.escapeSpaces("TestLname")
-							+ " "
-							+ utils.Utils.escapeSpaces("12345"));
-
-		String result = db.runQuery("SELECT COUNT(*) FROM systemUser WHERE username = '" + rndStr + "'");
-		assertEquals(Integer.parseInt("" + result.charAt(0)),1);
-=======
 		Database db = new Database(utils.Configuration.settings.get("DBConnection"));
 		db.execute("nonexistingname", "password", utils.Configuration.settings.get("PassCheck"));
 		assertTrue(db.getStatus(Database.Status.NONEXISTENT_USER));
@@ -74,10 +34,10 @@ public class TestDatabase
 	{
 		utils.Configuration.loadDefaultConfiguration();
 		Database db = new Database(utils.Configuration.settings.get("DBConnection"));
-		User rand_user = addRandUserToDatabase();
+		User random_user = addRandomUserToDatabase();
 
 		java.sql.PreparedStatement prep_statement = db.getPreparedStatement("SELECT COUNT(*) FROM systemUser WHERE username=?");
-		prep_statement.setString(1, rand_user.username);
+		prep_statement.setString(1, random_user.username);
 		String result = Database.resultToString(prep_statement.executeQuery());
 
 		assertEquals(Integer.parseInt("" + result.charAt(0)), 1);
@@ -91,7 +51,7 @@ public class TestDatabase
 	{
 
 		utils.Configuration.loadDefaultConfiguration();
-		setUp();
+		setup();
 		
 		db.executeWithValidUser
 		(
@@ -116,60 +76,34 @@ public class TestDatabase
 	{
 		utils.Configuration.loadDefaultConfiguration();
 		Database db = new Database(utils.Configuration.settings.get("DBConnection"));
-		User rand_user = addRandUserToDatabase();
+		User random_user = addRandomUserToDatabase();
+		String new_password = "newPW";
 
 		db.executeWithValidUser
 		(
-			rand_user, 
+			random_user, 
 			utils.Configuration.settings.getAndEscape("ChangePassCommand")
 			+ " " 
-			+ utils.Utils.escapeSpaces("newPW")
+			+ utils.Utils.escapeSpaces(new_password)
 		);
 
-		java.sql.PreparedStatement prep_statement = db.getPreparedStatement("SELECT hashedPW FROM systemUser WHERE username =?");
-		prep_statement.setString(1, rand_user.username);
-		String result = Database.resultToString(prep_statement.executeQuery());
-		String[] parts = result.split(" ");
-		assertTrue(PasswordHash.validatePassword("newPW", parts[1]));
->>>>>>> 81b82de93d1347b2c2ee06c6994984d352d27618
+		java.sql.PreparedStatement prep_statement = db.getPreparedStatement("SELECT hashedPW FROM systemUser WHERE username=?");
+		prep_statement.setString(1, random_user.username);
+		java.util.ArrayList<String> result = utils.Utils.splitAndUnescapeString(Database.resultToString(prep_statement.executeQuery()));
+		assertTrue(PasswordHash.validatePassword(new_password, result.get(Integer.parseInt(result.get(0)) + 1)));
 	}
 	
 	//@org.junit.Rule
   	//public org.junit.rules.ExpectedException exception = org.junit.rules.ExpectedException.none();
-
-<<<<<<< HEAD
-	@org.junit.Test 
-	public void testAddingDuplicateUserShouldFail() throws Exception
-	{
-
-		utils.Configuration.loadDefaultConfiguration();
-		User userR = new User(0,1,"root", "" ,"",PasswordHash.createHash("root"));
-		Database db = new Database(utils.Configuration.settings.get("DBConnection"));
-
-		assertEquals("It's likely that the user you're trying to add (root) already exists.",db.executeWithValidUser(userR, 
-							utils.Configuration.settings.getAndEscape("RegisterCommand")
-							+ " "
-							+ utils.Utils.escapeSpaces("root")
-							+ " "
-							+ utils.Utils.escapeSpaces("1")
-							+ " "
-							+ utils.Utils.escapeSpaces("TestFname")
-							+ " "
-							+ utils.Utils.escapeSpaces("TestLname")
-							+ " "
-							+ utils.Utils.escapeSpaces("12345")));
-	}
-
-=======
 	@org.junit.Test
 	public void testUserCannotChangeOthersPass() throws Exception
 	{
 		utils.Configuration.loadDefaultConfiguration();
-		setUp();
-		User rand_user = addRandUserToDatabase();
+		setup();
+		User random_user = addRandomUserToDatabase();
 		db.executeWithValidUser
 		(
-			rand_user,
+			random_user,
 			utils.Configuration.settings.getAndEscape("ChangePassOfCommand")
 			+ " "
 			+ utils.Utils.escapeSpaces("root")
@@ -182,29 +116,29 @@ public class TestDatabase
 	@org.junit.Test
 	public void testRootCanChangeOthersPass() throws Exception
 	{
-		User rand_user = addRandUserToDatabase();
-		setUp();
+		User random_user = addRandomUserToDatabase();
+		setup();
+		String new_password = "newPW";
 		db.executeWithValidUser
 		(
 			user_root,
 			utils.Configuration.settings.getAndEscape("ChangePassOfCommand")
 			+ " "
-			+ utils.Utils.escapeSpaces(rand_user.username)
+			+ utils.Utils.escapeSpaces(random_user.username)
 			+ " "
-			+ utils.Utils.escapeSpaces("newPW")
+			+ utils.Utils.escapeSpaces(new_password)
 		);
-		java.sql.PreparedStatement prep_statement = db.getPreparedStatement("SELECT hashedPW FROM systemUser WHERE username =?");
-		prep_statement.setString(1, rand_user.username);
-		String result = Database.resultToString(prep_statement.executeQuery());
-		String[] parts = result.split(" ");
-		assertTrue(PasswordHash.validatePassword("newPW", parts[1]));
+		java.sql.PreparedStatement prep_statement = db.getPreparedStatement("SELECT hashedPW FROM systemUser WHERE username=?");
+		prep_statement.setString(1, random_user.username);
+		java.util.ArrayList<String> result = utils.Utils.splitAndUnescapeString(Database.resultToString(prep_statement.executeQuery()));
+		assertTrue(PasswordHash.validatePassword(new_password, result.get(Integer.parseInt(result.get(0)) + 1)));
 	}
 
 	@org.junit.Test
 	public void testAddNewEventCommand() throws Exception
 	{
 		utils.Configuration.loadDefaultConfiguration();
-		setUp();
+		setup();
 
 		String rand_str = utils.Utils.makeRandomString(10);
 		db.executeWithValidUser
@@ -231,13 +165,13 @@ public class TestDatabase
 	public void testFindSystemUser() throws Exception
 	{
 		utils.Configuration.loadDefaultConfiguration();
-		setUp();
-		User rand_user = addRandUserToDatabase();
+		setup();
+		User random_user = addRandomUserToDatabase();
 
 
 		db.executeWithValidUser
 		(
-			rand_user,
+			random_user,
 			utils.Configuration.settings.getAndEscape("FindPersonCommand")
 			+ " "							
 			+ utils.Utils.escapeSpaces("name")
@@ -250,7 +184,7 @@ public class TestDatabase
 		assertTrue(result.length() > 2 );
 	}
 
-	private void setUp() throws Exception
+	private void setup() throws Exception
 	{
 		utils.Configuration.loadDefaultConfiguration();
 		db = new Database(utils.Configuration.settings.get("DBConnection"));
@@ -263,18 +197,18 @@ public class TestDatabase
 		}
 	}
 
-	private User addRandUserToDatabase() throws Exception
+	private User addRandomUserToDatabase() throws Exception
 	{
 		utils.Configuration.loadDefaultConfiguration();
 		User user_root = new User(1, 1, "root", "", "", PasswordHash.createHash("root"));
 		Database db = new Database(utils.Configuration.settings.get("DBConnection"));
-		String rand_str = utils.Utils.makeRandomString(8);
+		String random_str = utils.Utils.makeRandomString(8);
 		db.executeWithValidUser
 		(
 			user_root, 
 			utils.Configuration.settings.getAndEscape("RegisterCommand")
 			+ " "
-			+ utils.Utils.escapeSpaces(rand_str)
+			+ utils.Utils.escapeSpaces(random_str)
 			+ " "
 			+ utils.Utils.escapeSpaces("1")
 			+ " "
@@ -285,13 +219,18 @@ public class TestDatabase
 			+ utils.Utils.escapeSpaces("12345")
 		);
 
-		java.sql.PreparedStatement prep_statement = db.getPreparedStatement("SELECT systemUserId FROM systemUser WHERE username =?");
-		prep_statement.setString(1, rand_str);
-		String rand_str_userid = Database.resultToString(prep_statement.executeQuery());
+		java.sql.PreparedStatement prep_statement = db.getPreparedStatement("SELECT * FROM systemUser WHERE username=?");
+		prep_statement.setString(1, random_str);
+		String selection_result = Database.resultToString(prep_statement.executeQuery());
 
-		String[] parts = rand_str_userid.split(" ");
-		User rndUser = new User(Integer.parseInt("" + parts[1]), 5, rand_str, "", "", PasswordHash.createHash("12345"));
-		return  rndUser;
+		// String[] parts = rand_str_userid.split(" "); 
+		////////////////////////////////////////////////////////////
+		// DANGEROUS ^. DO NOT SPLIT RAW. //////////////////////////
+		////////////////////////////////////////////////////////////
+		java.util.ArrayList<String> parts = utils.Utils.splitAndUnescapeString(selection_result);
+		int columns = Integer.parseInt(parts.get(0));
+		parts = utils.Utils.splitAndUnescapeString(parts.get(columns + 1));
+		return new User(Integer.parseInt(parts.get(0)), Integer.parseInt(parts.get(1)), random_str, parts.get(2), parts.get(3), parts.get(4));
 	}
 }
 
@@ -308,5 +247,3 @@ public class TestDatabase
 }
 =======
 */
-
->>>>>>> 81b82de93d1347b2c2ee06c6994984d352d27618
