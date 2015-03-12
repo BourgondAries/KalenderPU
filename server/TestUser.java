af22232a4799de37d65b1bb367a753994978bf21
Edvard <hafskolt@gmail.com>
2015-03-12 15:48:40 +0100

    Changes to user and makefile

--- server/TestUser.java
+++ server/TestUser.java
@@ -5,44 +5,99 @@ import static org.junit.Assert.*;
 
 public class TestUser
 {
+/*
+	Database db = null;
+	User random_user = null;
+	User user_root = null;
+ */
+/*
+static 	int rank = 1, user_id = 7;
+static 	String username = "oar",
+		fname = "Ole Andreas",
+		lname = "Ramsdal",
+		hashed_password = "hash";
 
+static User user;
 
-	public static void fieldsShouldBeSet() 
+	@org.junit.Test
+	public static void fieldsShouldBeSet()
 	{
-		int id = 0;
-		int rank = 1;
-		String u_name = "oar", fname = "Ole Andreas" , lname = "Ramsdal", hashedPW = "hash";
-
-		User user = new User(id,rank,u_name,fname,lname,hashedPW);
+		 user = new User(user_id,rank,username,fname,lname,hashed_password);
 
 		//Tests
-		assertEquals(id, user.user_id);
+		assertEquals(user_id, user.user_id); 
 		assertEquals(rank, user.rank);
-		assertEquals(u_name, user.username);
+		assertEquals(username, user.username);
 		assertEquals(fname, user.fname);
 		assertEquals(lname, user.lname);
-		assertEquals(hashedPW, user.hashedPW);
+		assertEquals(hashed_password, user.hashed_password);
 	}
-	public static void getUserShouldReturnUser()
-	{
-		int id = 0;
-		int rank = 1;
-		String u_name = "oar", fname = "Ole Andreas" , lname = "Ramsdal", hashedPW = "hash";
 
-		User user = new User(id,rank,u_name,fname,lname,hashedPW);
+	public static void setFields(User user) 
+	{
+		user.user_id = user_id;
+		user.rank = rank;
+		user.username = username;
+		user.fname = fname; 
+		user.lname = lname;
+		user.hashed_password = hashed_password;
+	}
+	public static void setFields(User user, int user_id, int rank, String username, String firstname, String lastname, String hashed_password) 
+	{
+		user.user_id = user_id;
+		user.rank = rank;
+		user.username = username;
+		user.fname = firstname; 
+		user.lname = lastname;
+		user.hashed_password = hashed_password;
+	}
+	
+
+	@org.junit.Test
+	public static void getUserShouldReturnUser() throws java.sql.SQLException
+	{
+		User user = new User(user_id,rank,username,fname,lname,hashed_password);
 		assertEquals(user,user.getUser());
-
 	}
-	public static void getUserFromDatabse()
+
+/*
+	@org.junit.Test
+	
+	public static void getUserFromDatabase(User user) throws java.sql.SQLException
 	{
+		setFields(user);
+		user.saveNewUser(hashed_password);
 
+		//user.updateSystemUser();
+		setFields(user, 666, 2, "feilUsername", "feilFirstname", "feilLastname", "feilPassord" );
+		
+		try 
+		{
+			user.loadSystemUser(user_id);
+		} 
+		catch(java.sql.SQLException exc)
+		{
+			throw new java.sql.SQLException(exc);
+		}
+
+		assertEquals(user_id, user.user_id); 
+		assertEquals(rank, user.rank);
+		assertEquals(username, user.username);
+		assertEquals(fname, user.fname);
+		assertEquals(lname, user.lname);
+		assertEquals(hashed_password, user.hashed_password);
+		
 	}
-	public static void main(String[] args)
-
+	
+	public static void main(String[] args) throws java.sql.SQLException
 	{
 		//utils.Configuration.loadDefaultConfigurations();
-		fieldsShouldBeSet();
-		getUserShouldReturnUser();
+		User user = new User(user_id, rank, username, fname, lname, hashed_password);
+		System.out.println("Hello Test"); 
+		user.saveNewUser("hash"); 
+		
+		//getUserFromDatabase(user); 
 	}
+	*/
 }
 
