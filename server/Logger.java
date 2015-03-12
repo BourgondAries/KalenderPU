b4a0ca18da813f2189341a1b87b1f825d2c694db
allandd <allandd@stud.ntnu.no>
2015-03-12 11:17:48 +0100

    Ferdig med Logger

--- server/Logger.java
+++ server/Logger.java
@@ -1,15 +1,53 @@
+package server;
 import java.io.FileWriter;
 import java.sql.Date;
-
-public enum Logger{
-	DEBUGGING, MESSAGING;
-
-	public static void Log(String message, Logger type)
+import java.util.Calendar;
+import java.io.*;
+public class Logger{
+	
+	public static void Log(String message, String type, String stringSeverity) throws IOException, IllegalArgumentException
 	{
-		FileWriter fw = new FileWriter(utils.Configuration.settings.get("logfile") , true);
-		Date date = new Date();
-		fw.write(type +": " + message + " @" +date+ "\n");
+		ErrorType errorType;
+		Severity severity;
+		try 
+		{
+			errorType = ErrorType.valueOf(type);
+		}
+		catch (IllegalArgumentException e )
+		{
+			errorType = ErrorType.UNDEFINED;
+		}
 
+		try 
+		{
+			severity = Severity.valueOf(stringSeverity);
+		}
+		catch (IllegalArgumentException e)
+		{
+			severity = Severity.UNDEFINED;
+		}
+		
+		try
+		{
+		FileWriter fw = new FileWriter(utils.Configuration.settings.get("logfile") , true);
+		
+		java.sql.Date timeNow = new Date(Calendar.getInstance().getTimeInMillis());
+
+		fw.write("" + timeNow + ": " + errorType +", " + severity +", " + message + "\n");
+		fw.flush();
+		fw.close();
+		}
+
+		catch(IOException e)
+		{
+			System.err.println("Error: " + e.getMessage());		
+		}
+		
+		
+
+		
 	}
+
+
 
 }
